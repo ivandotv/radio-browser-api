a6970d38a05e119a4076f1d22e7bb6f29f330957
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-09-03 21:23:50 +0200

    feat: initial api setup

---
+++ src/radioBrowser.ts
@@ -0,0 +1,215 @@
+import {
+  StationSearchType,
+  AdvancedStationQuery,
+  CountryResult,
+  CountryStateResult,
+  Query,
+  Station,
+  StationQuery
+} from './constants'
+
+// todo list of station checks
+// todo list of station clicks
+
+export class RadioBrowser {
+  protected baseUrl = 'https://fr1.api.radio-browser.info/json'
+
+  protected fetchConfig: RequestInit = {
+    method: 'GET',
+    redirect: 'follow'
+  }
+
+  constructor(protected userAgent: string, protected fetchImpl: typeof fetch) {
+    this.fetchConfig.headers = { 'user-agent': this.userAgent }
+  }
+
+  async resolveBaseUrl(
+    autoSet = true,
+    config: RequestInit = {}
+  ): Promise<{ ip: string; name: string }> {
+    // alternative 'http://all.api.radio-browser.info/json/servers',
+
+    const result = await this.runRequest<{ name: string; ip: string }[]>(
+      this.buildRequest('servers'),
+      config
+    )
+    if (autoSet) {
+      this.baseUrl = result[0].ip
+    }
+
+    return result[0]
+  }
+
+  //
+  setBaseUrl(url: string): void {
+    this.baseUrl = url
+  }
+
+  getBaseUrl(): string {
+    return this.baseUrl
+  }
+
+  protected buildRequest(
+    endPoint: string,
+    search?: string,
+    query?: object
+  ): string {
+    search = search ? `/${encodeURIComponent(search)}` : ''
+    const queryParams = query ? this.createQueryParams(query) : ''
+
+    return `${this.baseUrl}/${endPoint}${search}${queryParams}`
+  }
+
+  protected async runRequest<T>(
+    url: string,
+    config: RequestInit = {}
+  ): Promise<T> {
+    // manual merge deep
+    const headers = Object.assign({}, this.fetchConfig.headers, config.headers)
+    const fetchConfig = Object.assign({}, this.fetchConfig, config)
+    fetchConfig.headers = headers
+
+    const response = await this.fetchImpl(url, fetchConfig)
+
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw response
+    }
+  }
+
+  async getCountries(
+    search?: string,
+    query?: Query,
+    fetchConfig?: RequestInit
+  ): Promise<CountryResult[]> {
+    return this.runRequest(
+      this.buildRequest('countries', search, query),
+      fetchConfig
+    )
+  }
+
+  async getCountryCodes(
+    search?: string,
+    query?: Query,
+    fetchConfig?: RequestInit
+  ): Promise<CountryResult[]> {
+    search = search ? `/${search.toUpperCase()}` : ''
+
+    return this.runRequest(
+      this.buildRequest('countrycodes', search, query),
+      fetchConfig
+    )
+  }
+
+  async getCodecs(
+    search?: string,
+    query?: Query,
+    fetchConfig?: RequestInit
+  ): Promise<CountryResult[]> {
+    search = search ? `/${search.toUpperCase()}` : ''
+
+    return this.runRequest(
+      this.buildRequest('codecs', search, query),
+      fetchConfig
+    )
+  }
+
+  async getCountryStates(
+    search?: string,
+    query?: Query,
+    fetchConfig?: RequestInit
+  ): Promise<CountryStateResult[]> {
+    return this.runRequest(
+      this.buildRequest('states', search, query),
+      fetchConfig
+    )
+  }
+
+  async getLanguages(
+    search?: string,
+    config?: Query,
+    fetchConfig?: RequestInit
+  ): Promise<CountryResult[]> {
+    return this.runRequest(
+      this.buildRequest('languages', search, config),
+      fetchConfig
+    )
+  }
+
+  async getTags(
+    search?: string,
+    config?: Query,
+    fetchConfig?: RequestInit
+  ): Promise<CountryResult[]> {
+    search = search ? search.toLowerCase() : ''
+
+    return this.runRequest(
+      this.buildRequest('tags', search, config),
+      fetchConfig
+    )
+  }
+
+  // http://fr1.api.radio-browser.info/{format}/stations/byuuid/{searchterm}
+  async getStationsBy(
+    searchType: keyof typeof StationSearchType,
+    search?: string,
+    query?: StationQuery,
+    fetchConfig?: RequestInit
+  ): Promise<Station[]> {
+    if (!StationSearchType[searchType]) {
+      throw new Error(`search type does not exist: ${searchType}`)
+    }
+
+    search = search ? search.toLowerCase() : ''
+
+    return this.runRequest(
+      this.buildRequest('stations', `${searchType}/${search}`, query),
+      fetchConfig
+    )
+  }
+
+  async getAllStations(
+    query?: Omit<StationQuery, 'hideBroken'>,
+    fetchConfig?: RequestInit
+  ): Promise<Station[]> {
+    return this.runRequest(
+      this.buildRequest('stations', '', query),
+      fetchConfig
+    )
+  }
+
+  async sendStationClick(
+    uuid: string,
+    fetchConfig?: RequestInit
+  ): Promise<{
+    ok: boolean
+    message: string
+    stationuuid: string
+    name: string
+    url: string
+  }> {
+    return this.runRequest(this.buildRequest('url', uuid), fetchConfig)
+  }
+
+  async searchStations(
+    query: AdvancedStationQuery,
+    fetchConfig?: RequestInit
+  ): Promise<Station[]> {
+    return this.runRequest(
+      this.buildRequest('search', undefined, query),
+      fetchConfig
+    )
+  }
+
+  protected createQueryParams(params?: object): string {
+    let result = ''
+    if (params) {
+      for (const [key, value] of Object.entries(params)) {
+        result += `&${key}=${encodeURIComponent(value)}`
+      }
+    }
+
+    return result ? `?${result.slice(1).toLowerCase()}.` : ''
+  }
+}
---
+++ tests/radioBrowser.test.ts
@@ -0,0 +1,150 @@
+import nock from 'nock'
+import nodeFetch from 'node-fetch'
+import { RadioBrowser } from '../src/radioBrowser'
+
+const globalTest = {
+  fetch: (nodeFetch as unknown) as typeof fetch
+}
+
+describe('Radio Browser', () => {
+  afterEach(() => {
+    nock.cleanAll()
+    jest.clearAllMocks()
+  })
+
+  test('Resolve base url', async () => {
+    const mockResult = [
+      {
+        name: 'server name',
+        ip: '1.2.2.2'
+      }
+    ]
+
+    nock(/radio-browser.info/)
+      .get('/json/servers')
+      .reply(200, mockResult)
+
+    const api = new RadioBrowser('test', globalTest.fetch)
+    const result = await api.resolveBaseUrl()
+
+    expect(result).toEqual(mockResult[0])
+    expect(api.getBaseUrl()).toBe(mockResult[0].ip)
+  })
+
+  test('Resolve base url but do not set it', async () => {
+    const api = new RadioBrowser('test', globalTest.fetch)
+    const defaultBaseUrl = api.getBaseUrl()
+    const mockResult = [
+      {
+        name: 'server name',
+        ip: '1.2.2.2.'
+      }
+    ]
+
+    nock(/radio-browser.info/)
+      .get('/json/servers')
+      .reply(200, mockResult)
+
+    const result = await api.resolveBaseUrl(false)
+
+    expect(result).toEqual(mockResult[0])
+    expect(api.getBaseUrl()).toBe(defaultBaseUrl)
+  })
+
+  test('Manually set base url', () => {
+    const url = '1.2.3.4'
+
+    const api = new RadioBrowser('test', globalTest.fetch)
+    api.setBaseUrl(url)
+
+    expect(api.getBaseUrl()).toBe(url)
+  })
+
+  test('Throw if response is not OK', async () => {
+    const errorText = 'server error'
+    nock(/radio-browser.info/)
+      .get('/json/servers')
+      .reply(500, errorText)
+
+    const api = new RadioBrowser('test', globalTest.fetch)
+    expect.assertions(1)
+    try {
+      await api.resolveBaseUrl()
+    } catch (e) {
+      const result = await e.text()
+      expect(result).toBe(errorText)
+    }
+  })
+
+  test('When fetching, custom user agent is present', async () => {
+    nock(/radio-browser.info/)
+      .get('/json/servers')
+      .reply(200, [{ name: '', ip: '' }])
+
+    const agent = 'test'
+    const spy = jest.spyOn(globalTest, 'fetch')
+    const api = new RadioBrowser(agent, globalTest.fetch)
+
+    await api.resolveBaseUrl()
+
+    expect(spy).toBeCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        headers: expect.objectContaining({ 'user-agent': agent })
+      })
+    )
+  })
+
+  test('When fetching, custom headers are present', async () => {
+    nock(/radio-browser.info/)
+      .get('/json/servers')
+      .reply(200, [{ name: '', ip: '' }])
+
+    const api = new RadioBrowser('test', globalTest.fetch)
+    const spy = jest.spyOn(globalTest, 'fetch')
+
+    const headerName = 'x-jest-test'
+    const headerValue = '1'
+
+    await api.resolveBaseUrl(true, {
+      headers: {
+        [headerName]: headerValue
+      }
+    })
+
+    expect(spy).toBeCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        headers: expect.objectContaining({ [headerName]: headerValue })
+      })
+    )
+  })
+
+  test('get countries', async () => {
+    nock(/radio-browser.info/)
+      .get(/json\/countries/)
+      .reply(200, {})
+
+    const api = new RadioBrowser('test', globalTest.fetch)
+    const spy = jest.spyOn(globalTest, 'fetch')
+    const headerName = 'x-jest-test'
+    const headerValue = '1'
+
+    await api.getCountries(
+      'search',
+      { order: 'name' },
+      {
+        headers: {
+          [headerName]: headerValue
+        }
+      }
+    )
+
+    expect(spy).toBeCalledWith(
+      expect.stringMatching(/countries\/search\?order=name/),
+      expect.objectContaining({
+        headers: expect.objectContaining({ [headerName]: headerValue })
+      })
+    )
+  })
+})
